399c8f3a3a9fd66b6bb848ee24ca4a06c607cf24
Kenia <kap-araujo@outlook.com>
2018-04-17 18:01:36 -0300

    Melhorias de interface

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -29,18 +29,35 @@
             width: 400px;
             
         }
+        .py-4 {
+            margin-top: 50px;
+        }
     </style>
 </head>
 <body>
     <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
+        <nav class="navbar navbar-expand-md navbar-dark bg-dark navbar-laravel fixed-top">
             <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
+                <a class="navbar-brand" href="{{ url('/home') }}">
                     {{ config('app.name', 'Agenda') }}
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
+                <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="{{ route('tarefas/tarefasCreate') }}">Nova Tarefa <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item active">
+                        <a class="nav-link" href="{{ route('pessoas/pessoasCreate') }}">Novo Contato <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item active">
+                        <a class="nav-link" href="{{ route('tarefas/tarefasList') }}">Minhas Tarefas <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item active">
+                        <a class="nav-link" href="{{ route('pessoas/pessoasList') }}">Meus Contatos <span class="sr-only">(current)</span></a>
+                    </li>
+                </ul>
 
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
--- resources/views/pessoas/pessoaslist.blade.php
+++ resources/views/pessoas/pessoaslist.blade.php
@@ -6,18 +6,21 @@
         <div class="col-md-8">
             <div class="card">
                 <div class="card-header">Seus Contatos</div>
-
                 <div class="card-body">
                 @forelse($pessoas as $pessoas)
                 <ul class="list-group">
                     <li class="list-group-item">
+                    <div class="col-md-8">
                         <a href="{{ route('pessoas/pessoasShow', $pessoas) }}">{{ $pessoas->nome}} - {{ $pessoas->telefone}}</a>
+                    <div class="col-md-8">
+                    <div class="col-md-4">
                         <form action="{{ route('pessoas/pessoasDelete', $pessoas->id) }}" method="POST">
                         {{ csrf_field() }}
                         {{ method_field('DELETE') }}
                             <input type="hidden" name="_method" value="DELETE">
                             <button class="btn btn-danger">Deletar</button>
                         </form>
+                    </div>
                         <a href="{{ route('pessoas/pessoasEdit', $pessoas->id) }}">Editar</a>
                     </li>
                 </ul>
